45d1e55828e365a334dc7718eb3068ed0670ccc2
IceBlueX <baokun.li@orgneering.com>
2018-12-21 10:21:20 +0800

    some

--- 8 错误、调试和测试/调试.py
+++ 8 错误、调试和测试/调试.py
@@ -41,3 +41,12 @@ n = int(s)
 logging.info('n = %d' % n)
 print(10 / n)
 
+#这就是logging的好处，它允许你指定记录信息的级别
+#有debug, info, warning, error等几个级别
+#当指定info时，debug就不起作用了，指定WARNING时，gebug和info就不起作用了
+#logging的另一个好处是通过简单的配置，
+#一条语句可以同时输出到不同的地方，比如console和文件
+
+
+
+
